3387b170b7f7ec86292a4c47ea38c3cfcc41bd92
Ефимов Максим <efimov_m@itstep.academy>
2023-03-19 14:45:03 +0300

    19-03-23

--- NetNullableTypesApp/Program.cs
+++ NetNullableTypesApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Nullable<Int32> n;
+using System.Net.Sockets;
+
+int? n;
+n = null;
+
+int m = n ?? 100;
+
+//string s = "Hello";
+//Console.WriteLine(s);
+
+User user = new User() { Name = "Bob" };
+
+Console.Write($"User name: {user.Name}");
+if (user.Company is not null)
+{
+    Console.Write($", company title: {user.Company.Title}");
+    if (user.Company.Address is not null)
+        Console.Write($", company city: {user.Company.Address.City}");
+}
+Console.WriteLine();
+
+Console.Write($"User name: {user.Name}, company title: {user.Company?.Title}, company city: {user.Company?.Address?.City}");
+
+
+class User
+{
+    public string? Name { get; set; }
+    public Company? Company { get; set; }
+
+}
+
+class Company
+{
+    public string Title { set; get; } = null!;
+    public Address? Address { set; get; }
+
+}
+
+class Address
+{
+    public string? City { set; get; }
+}
